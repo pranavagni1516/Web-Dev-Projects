e3e71a17f9ed2ed2c3650baff0d5b291315202b7
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2023-06-01 01:17:45 +0530

    Update app.js

--- LinkTreeClone/app.js
+++ LinkTreeClone/app.js
@@ -1,4 +1,22 @@
-const shareButton = document.querySelectorAll('.tile-share-button','.share-button')
+const shareButton = document.querySelectorAll('.tile-share-button')
+console.log(shareButton)
+async function copyText(e){
+    e.preventDefault()
+    const link= this.getAttribute('link')
+    console.log(link)
+
+    try{
+        await navigator.clipboard.writeText(link)
+        alert("Copied the text: " + link)
+    } catch(err){
+        console.error(err)
+    }
+
+}
+
+shareButton.forEach(shareButton => shareButton.addEventListener('click',copyText))
+
+const shareButton = document.querySelectorAll('.share-button')
 console.log(shareButton)
 async function copyText(e){
     e.preventDefault()
