2d47b799ce5420da84718850075d208d69e91c60
pranavagni1516 <57935482+pranavagni1516@users.noreply.github.com>
2023-06-01 01:09:51 +0530

    Add files via upload

---
+++ LinkTreeClone/app.js
@@ -0,0 +1,17 @@
+const shareButton = document.querySelectorAll('.tile-share-button')
+console.log(shareButton)
+async function copyText(e){
+    e.preventDefault()
+    const link= this.getAttribute('link')
+    console.log(link)
+
+    try{
+        await navigator.clipboard.writeText(link)
+        alert("Copied the text: " + link)
+    } catch(err){
+        console.error(err)
+    }
+
+}
+
+shareButton.forEach(shareButton => shareButton.addEventListener('click',copyText))
